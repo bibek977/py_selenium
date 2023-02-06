38e30a6086e0c07ce64edb3f57b2ac783786ae0e
bibek977 <bhattaraibibek91@gmail.com>
2023-02-06 14:47:39 +0545

    pytest package

---
+++ package/test_assert.py
@@ -0,0 +1,16 @@
+import pytest
+
+@pytest.mark.runthis
+def test_assert_2():
+    assert 10 == 10, 'Hello bhattarai'
+  
+# @pytest.mark.skip
+@pytest.mark.xfail
+def test_assert():
+    assert 2 == 3, "Hello bibek"
+
+# simple run == pytest -s
+# single run == pytest test_assert -s
+# same keyword run == pytest -k asse -s
+# selective run == pytest -m runthis -s 
+# selective run == pytest -m runthis -s -v for more details
---
+++ package/test_print.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.runthis
+def test_print():
+    print("Test case")
